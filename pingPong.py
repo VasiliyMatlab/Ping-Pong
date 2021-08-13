757dd9e50999ef0a5738a5b192d4134feca718b0
VasiliyMatlab <vasiliy0399@yandex.ru>
2021-08-13 20:17:02 +0300

    Создание мячика
    - создал мячик
    - реализовал отражение от стенок
    - при попадании на левую или правую стенки перемещается на центральную линию со случайным разбросом по оси 'y' недалеко от центра
    https://www.youtube.com/watch?v=3P0lArajvu0&list=PLQAt0m1f9OHt4G8G016AJOQuX4u1MekE-&index=3

--- pingPong.py
+++ pingPong.py
@@ -1,5 +1,7 @@
 import turtle
+from random import choice, randint
 
+#
 #                              Поле:
 #                          LENGTH = 1000
 #   |<--------------------------------------------------------->|
@@ -43,7 +45,7 @@ border.goto(-LENGTH/2, -WIDTH/2)
 border.goto(-LENGTH/2, WIDTH/2)
 border.end_fill()
 
-# Отрисовка пунктирной линии в центре
+# Отрисовка пунктирных линий в центре
 NUM_GRID  = 25               # кол-во пунктирных отрезков
 GRID_STEP = WIDTH / NUM_GRID # длина пунктира
 border.goto(0, WIDTH/2)
@@ -68,6 +70,7 @@ if ROCKET_IDENT < (ROCKET_LENGTH / 2):
 
 # Создание левой ракетки
 rocket_left = turtle.Turtle()
+rocket_left.speed(0)
 rocket_left.color("white")
 rocket_left.shape("square")
 rocket_left.shapesize(stretch_len = ROCKET_LENGTH/PIX_PER_VALUE/2, \
@@ -77,12 +80,24 @@ rocket_left.goto(-LENGTH/2 + ROCKET_IDENT, 0)
 
 # Создание правой ракетки
 rocket_right = turtle.Turtle()
+rocket_right.speed(0)
 rocket_right.color("white")
 rocket_right.shape("square")
-rocket_right.shapesize(stretch_len = ROCKET_LENGTH/PIX_PER_VALUE/2, \
-    stretch_wid = ROCKET_WIDTH/PIX_PER_VALUE/2)
+rocket_right.shapesize(stretch_len = ROCKET_LENGTH/2/PIX_PER_VALUE, \
+    stretch_wid = ROCKET_WIDTH/2/PIX_PER_VALUE)
 rocket_right.penup()
 rocket_right.goto(LENGTH/2 - ROCKET_IDENT, 0)
+
+# Создание мячика
+DIAMETER = 20 # диаметр мячика
+ball = turtle.Turtle()
+ball.speed(0)
+ball.shape("circle")
+ball.shapesize(DIAMETER/2/PIX_PER_VALUE)
+ball.color("red")
+ball.penup()
+ball.dx = choice([-4,-3,-2, 2,3,4]) # ск-ть перемещения мячика по x
+ball.dy = choice([-4,-3,-2, 2,3,4]) # ск-ть перемещения мячика по y
 
 # Движение левой ракетки вверх
 def left_move_up():
@@ -117,5 +132,24 @@ window.onkeypress(left_move_up, 'w')
 window.onkeypress(left_move_down, 's')
 window.onkeypress(right_move_up, 'Up')
 window.onkeypress(right_move_down, 'Down')
+
+while True:
+    window.update()
+    
+    # Перемещение мячика
+    ball.setx(ball.xcor() + ball.dx)
+    ball.sety(ball.ycor() + ball.dy)
+    
+    # Отражение от верхней и нижней стенок
+    if ball.ycor() >= (WIDTH/2 - DIAMETER/2):
+        ball.dy = -ball.dy
+    elif ball.ycor() <= (-WIDTH/2 + DIAMETER/2):
+        ball.dy = -ball.dy
+    # При достижении левой или правой стенки возвращаемся в центр
+    elif (ball.xcor() <= (-LENGTH/2 + DIAMETER/2)) or \
+            (ball.xcor() >= (LENGTH/2 - DIAMETER/2)):
+        ball.goto(0, randint(-WIDTH/4, WIDTH/4))
+        ball.dx = choice([-4,-3,-2, 2,3,4])
+        ball.dy = choice([-4,-3,-2, 2,3,4])
 
 window.mainloop()
